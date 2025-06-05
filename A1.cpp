89a344edbe7fc7c8434eebd7e9703617a2c6ef7b
Siddharth Joshi <siddharthjoshi.42k6@gmail.com>
2025-06-05 23:24:38 +0530

    Add files via upload

---
+++ A1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {3, 2, 1, 5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    cout << "The largest element is: " << largest << endl;
+    return 0;
+}
